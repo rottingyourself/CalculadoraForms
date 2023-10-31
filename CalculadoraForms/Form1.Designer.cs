420c7cb532d3da3ea49b9b725d5092d27b0b1782
Habilidebob de Jesus <80491104+habilidebob@users.noreply.github.com>
2023-10-31 20:12:19 -0300

    Initial commit

---
+++ CalculadoraForms/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace CalculadoraForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnMais = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnVezes = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.txbTela = new System.Windows.Forms.TextBox();
+            this.txbAux = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn9
+            // 
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(168, 82);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(70, 60);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(92, 82);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(70, 60);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(16, 82);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 60);
+            this.btn7.TabIndex = 2;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(168, 148);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 60);
+            this.btn6.TabIndex = 3;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(92, 148);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 60);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(16, 148);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 60);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(168, 214);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 60);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(92, 214);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 60);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(16, 214);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 60);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn0.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(16, 280);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(146, 60);
+            this.btn0.TabIndex = 9;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.Numero_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.Red;
+            this.btnLimpar.Location = new System.Drawing.Point(168, 280);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(70, 60);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "C";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnMais
+            // 
+            this.btnMais.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMais.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMais.Location = new System.Drawing.Point(244, 214);
+            this.btnMais.Name = "btnMais";
+            this.btnMais.Size = new System.Drawing.Size(70, 60);
+            this.btnMais.TabIndex = 11;
+            this.btnMais.Text = "+";
+            this.btnMais.UseVisualStyleBackColor = true;
+            this.btnMais.Click += new System.EventHandler(this.Operador_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMenos.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenos.Location = new System.Drawing.Point(244, 171);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(70, 37);
+            this.btnMenos.TabIndex = 12;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.Operador_Click);
+            // 
+            // btnVezes
+            // 
+            this.btnVezes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVezes.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVezes.Location = new System.Drawing.Point(244, 128);
+            this.btnVezes.Name = "btnVezes";
+            this.btnVezes.Size = new System.Drawing.Size(70, 37);
+            this.btnVezes.TabIndex = 15;
+            this.btnVezes.Text = "x";
+            this.btnVezes.UseVisualStyleBackColor = true;
+            this.btnVezes.Click += new System.EventHandler(this.Operador_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDividir.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(244, 82);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(70, 40);
+            this.btnDividir.TabIndex = 16;
+            this.btnDividir.Text = "÷";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.Operador_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIgual.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.ForeColor = System.Drawing.Color.LawnGreen;
+            this.btnIgual.Location = new System.Drawing.Point(244, 280);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(70, 60);
+            this.btnIgual.TabIndex = 17;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // txbTela
+            // 
+            this.txbTela.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txbTela.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTela.Location = new System.Drawing.Point(16, 23);
+            this.txbTela.Name = "txbTela";
+            this.txbTela.ReadOnly = true;
+            this.txbTela.Size = new System.Drawing.Size(298, 53);
+            this.txbTela.TabIndex = 18;
+            this.txbTela.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txbAux
+            // 
+            this.txbAux.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txbAux.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbAux.Location = new System.Drawing.Point(16, 12);
+            this.txbAux.Name = "txbAux";
+            this.txbAux.ReadOnly = true;
+            this.txbAux.Size = new System.Drawing.Size(70, 31);
+            this.txbAux.TabIndex = 19;
+            this.txbAux.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(326, 364);
+            this.Controls.Add(this.txbAux);
+            this.Controls.Add(this.txbTela);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnVezes);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMais);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ":: Calculadora ::";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnMais;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnVezes;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.TextBox txbTela;
+        private System.Windows.Forms.TextBox txbAux;
+    }
+}
+
---
+++ CalculadoraForms/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraForms
+{
+    public partial class Form1 : Form
+    {
+        int numero1;
+        string ultimoOp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txbTela.Clear();
+        }
+        private void Operador_Click(object sender, EventArgs e)
+        {
+            // Obter o botão que está chamando o evento
+            var botao = (Button)sender;
+            numero1 = int.Parse(txbTela.Text);
+            txbTela.Clear();
+            txbAux.Text = numero1.ToString() + botao.Text;
+            ultimoOp = botao.Text;
+
+        }
+        private void Numero_Click(object sender, EventArgs e)
+        {
+            // Obter o botão que está chamando o evento
+            var botao = (Button)sender;
+            txbTela.Text += botao.Text;
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            switch (ultimoOp)
+            {
+                case "+":
+                    txbAux.Clear();
+                    txbTela.Text = (numero1 + int.Parse(txbTela.Text)).ToString();
+                    break;
+            }
+        }
+    }
+}
